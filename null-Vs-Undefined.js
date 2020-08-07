b56a0704dcbea8b98335feea3d9b8235ae334a88
iqbal-dev <56572441+iqbal-dev@users.noreply.github.com>
2020-08-07 07:40:09 +0600

    advance js

---
+++ null-Vs-Undefined.js
@@ -0,0 +1,25 @@
+
+function add(num1,num2) {
+    console.log(num1 + num2);
+}
+const result = add(5, 4);
+console.log(result)
+
+function add(num1,num2) {
+    return
+}
+console.log(add(10, 20));
+
+function add(num1,num2) {
+    console.log(num2);
+}
+add(20)
+
+const obj = {
+    name: 'Md iqbal hossain',
+    phone: '01792119624'
+}
+console.log(obj.address);
+
+const array = [12, 34, 45, 54];
+console.log(array[5])
--- truthyAndFalsy.js
+++ truthyAndFalsy.js
@@ -1,76 +1,76 @@
 
-//    0
-// const number = 0;
-// if (number) {
-//     console.log('number is true');  
-// }
-// else {
-//     console.log('number is false');
-// }
+   0
+const number = 0;
+if (number) {
+    console.log('number is true');  
+}
+else {
+    console.log('number is false');
+}
 
 
-// -0
-// const number = -0;
-// if (number) {
-//     console.log('number is true');
-// }
-// else {
-//     console.log('number is false');
-// }
+-0
+const number = -0;
+if (number) {
+    console.log('number is true');
+}
+else {
+    console.log('number is false');
+}
 
 
-// const number = -1;
-// if (number) {
-//     console.log('number is true');
-// }
-// else {
-//     console.log('number is false');
-// }
+const number = -1;
+if (number) {
+    console.log('number is true');
+}
+else {
+    console.log('number is false');
+}
 
-//        0n
-// const number = 0n;
-// if (number) {
-//     console.log('number is true');
-// }
-// else {
-//     console.log('number is false');
-// }
-
-
-//        null
-// const number = null;
-
-// if (number) {
-//     console.log('number is true');
-// }
-// else {
-//     console.log('number is false');
-// }
-
-//         false
-// const number = false;
-// if (number) {
-//     console.log('number is true');
-// }
-// else {
-//     console.log('number is false');
-// }
-
-//         undefined
-// const number = undefined;
-// if (number) {
-//     console.log('number is true');
-// }
-// else {
-//     console.log('number is false');
-// }
+       0n
+const number = 0n;
+if (number) {
+    console.log('number is true');
+}
+else {
+    console.log('number is false');
+}
 
 
-// NaN
-// const number = NaN;
-// if (number) {
-//     console.log('number is true');
-// }
-// else {
-//     console.log('number is false');
-// }
+       null
+const number = null;
+
+if (number) {
+    console.log('number is true');
+}
+else {
+    console.log('number is false');
+}
+
+        false
+const number = false;
+if (number) {
+    console.log('number is true');
+}
+else {
+    console.log('number is false');
+}
+
+        undefined
+const number = undefined;
+if (number) {
+    console.log('number is true');
+}
+else {
+    console.log('number is false');
+}
+
+
+NaN
+const number = NaN;
+if (number) {
+    console.log('number is true');
+}
+else {
+    console.log('number is false');
+}
