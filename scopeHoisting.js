998eb00173b1b7b6f0c25894471409a423103acd
iqbal-dev <56572441+iqbal-dev@users.noreply.github.com>
2020-08-07 10:03:22 +0600

    advance js

---
+++ scopeHoisting.js
@@ -0,0 +1,12 @@
+
+function myfunc() {
+    let name = 'md Iqbal hossain';
+}
+console.log(name);//here name variable is at myfunc function it means name is an myfunc Block. so that name variable is not available outside of the block
+
+
+console.log(day);//here day is declare so that here output is undefined but value is assigned.because day are in creation phase and that is store in a memory but not assigned a value so that day have undefined value;
+
+var day = 'friday';
+
+
